f029a28836bf26096264e517ed5a407922b6d944
Ian Gardner <ian.gardner@siemens.com>
2023-07-24 23:43:44 +0100

    test loging stuff

--- src/App.js
+++ src/App.js
@@ -13,11 +13,21 @@ function App() {
   return <div>
     <div>
     <a href="/.auth/login/google?post_login_redirect_uri=https://jwt.ms">Login with Google</a>
+    </div>
+    <div>
     <a href="/.auth/login/facebook?post_login_redirect_uri=https://jwt.ms">Login with Facebook</a>
+    </div>
+    <div>
     <a href="/.auth/login/twitter?post_login_redirect_uri=https://jwt.ms">Login with Twitter</a>
+    </div>
+    <div>
     <a href="/.auth/login/github?post_login_redirect_uri=https://jwt.ms">Login with Github</a>
+    </div>
+    <div>
     <a href="/.auth/login/aad?post_login_redirect_uri=https://jwt.ms">Login with Microsoft</a>
-    <a href="/.auth/logout?post_logout_redirect_uri=https://www.google.co.uk">Logout</a>
+    </div>
+    <div>
+    <a href="/.auth/logout">Logout</a>
     </div>
     <div>
       {data}
